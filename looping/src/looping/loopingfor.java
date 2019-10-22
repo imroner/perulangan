1718828fd4ba505f2316f9767633a9e8694119a1
imroner <46581220+imroner@users.noreply.github.com>
2019-10-22 19:44:09 +0700

    Add files via upload

---
+++ looping/src/looping/loopingfor.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package looping;
+
+/**
+ *
+ * @author Moch Lutfi
+ */
+public class loopingfor {
+    public static void main(String[] args) {
+    for (int i = 0; i < 5; i=i+1) {
+      System.out.println(i);
+    }  
+  }
+}
---
+++ looping/src/looping/loopingforstring.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package looping;
+
+/**
+ *
+ * @author Moch Lutfi
+ */
+public class loopingforstring {
+    public static void main(String[] args) {
+    String[] keluargaku = {"Amin", "ANa", "Agus", "Arsa"};
+    for (String i : keluargaku) {
+      System.out.println(i);
+    }    
+  }
+}
